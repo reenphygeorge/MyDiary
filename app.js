01d0a0280d6ec845d75eea77ea5fd848fdfcbc0a
reenphygeorge <reenphygeorge@gmail.com>
2022-02-22 23:15:01 +0530

    Updated

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
-
+const bodyParser = require('body-parser')
 const path = require('path');
 
 const handlebars = require('express-handlebars');
+
+const fs = require('fs');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view-engine', 'hbs');
@@ -13,25 +15,36 @@ app.engine('hbs', handlebars.engine({
     defaultLayout: 'layout'
 }));
 app.use(express.static('public'));
-app.get('/home', (req,res) => {
-    res.render('home.hbs')
-})
-app.get('/add', (req,res) => {
-    res.render('add.hbs')
-})
-app.get('/view', (req,res) => {
-    res.render('view.hbs')
-})
-app.get('/signin', (req,res) => {
-    res.render('signin.hbs')
-})
-app.get('/signup', (req,res) => {
-    res.render('signup.hbs')
-})
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.get('/home', (req, res) => {
+    res.status(200).render('home.hbs')
+});
+app.get('/add', (req, res) => {
+    res.status(200).render('add.hbs')
+});
+app.post('/add', (req, res) => {
+    let path = "data/"+req.body.diaryDate+".md"
+    fs.writeFile(path, req.body.diaryContent, (err) => {
+        if(err)
+            console.log(err)
+    })
+    res.status(200).render('success.hbs')
+});
+app.get('/view', (req, res) => {
+    res.status(200).render('view.hbs')
+});
+app.get('/signin', (req, res) => {
+    res.status(200).render('signin.hbs')
+});
+app.get('/signup', (req, res) => {
+    res.status(200).render('signup.hbs')
+});
 app.all('*', (req, res) => {
     res.status(404).render('404.hbs');
 });
-
-
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server listening at Port ${port}`));
